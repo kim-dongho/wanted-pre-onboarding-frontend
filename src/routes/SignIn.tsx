58464e768b03b6b9f1502511dc22240f8a5b0d4a
kim-dongho <dongho_k@naver.com>
2023-07-06 17:51:07 +0900

    test

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -33,7 +33,7 @@ const SignIn = () => {
     <Container>
       <SignInWrapper>
         <Header>
-          <Title>Sign In1</Title>
+          <Title>Sign In123123</Title>
         </Header>
         <AuthForm />
         <ChangeSignRoute />
